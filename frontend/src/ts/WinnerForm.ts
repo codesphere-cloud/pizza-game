a96664509236f525b9fa3d771ff38fc93ad673dd
Jonas Zipprick <jonas.zipprick@gmail.com>
2020-11-25 11:28:55 +0100

    Improves the response parsing of the winner form.

--- frontend/src/ts/WinnerForm.ts
+++ frontend/src/ts/WinnerForm.ts
@@ -1,3 +1,5 @@
+import {Reply} from "pizza-game-backend/lib/helpers/Reply";
+
 export class WinnerForm {
     private readonly element: Element;
 
@@ -6,10 +8,17 @@ export class WinnerForm {
     }
 
     public async show(): Promise<void> {
-        const giftCode = await fetch('backend/gift-code', {
-            method: 'GET',
-        });
-        this.element.querySelector(".gift-code")!.innerHTML = `Your gift code: ${await giftCode.text()}`
+        const giftCode = Reply.createFromSerializedReply(
+            JSON.parse(
+                await (
+                    await fetch('backend/gift-code', {
+                        method: 'GET',
+                    })
+                ).text()
+            )
+        ).logIfError().getValue();
+
+        this.element.querySelector(".gift-code")!.innerHTML = `Your gift code: ${giftCode}`;
         this.element.classList.remove("hidden");
     }
 }
